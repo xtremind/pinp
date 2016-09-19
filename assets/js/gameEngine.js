b7f96645c3754aff3feccdac9dae1579c5568c87
Xtremind <guillaume.dagraca@gmail.com>
2016-09-19 22:54:15 +0200

    load and display logo

--- assets/js/gameEngine.js
+++ assets/js/gameEngine.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-	var game = new Phaser.Game(800,400, Phaser.AUTO,'gameContainer');
+var game = new Phaser.Game(800,400, Phaser.AUTO,'gameContainer', {
+	preload: preload, create: create
 });
+
+function preload(){
+	game.load.image('logo','/assets/img/phaser.png');
+};
+
+function create(){
+	game.add.sprite(10,20,'logo');
+};
+function update(){
+	
+};
+
+
+
