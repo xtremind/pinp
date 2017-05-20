3c06fbc24d37a7f338b05bd5296b76325fca60c4
Xtremind <guillaume.dagraca@gmail.com>
2017-05-20 18:11:10 +0200

    change ghost image

--- assets/js/Preloader.js
+++ assets/js/Preloader.js
@@ -4,10 +4,6 @@ Game.Preloader = function (game) {
 
 Game.Preloader.prototype = {
     preload : function () {
-        //this.preloadBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBar');
-        //this.preloadBar.anchor.setTo(0.5, 0.5);
-        //this.time.advancedTiming = true;
-        //this.load.setPreloadSprite(this.preloadBar);
         
         // add a 'Loading ...' label on the screen
         var loadingLabel = this.add.text(this.world.centerX, 150, 'Loading ...', {font: '30px Arial', fill: '#ffffff'});
@@ -24,7 +20,7 @@ Game.Preloader.prototype = {
         this.load.image('tileset', 'assets/img/tileset_map2.png');
         
         this.load.spritesheet('player', 'assets/img/player.png');
-        this.load.spritesheet('phantom', 'assets/img/player.png');
+        this.load.spritesheet('phantom', 'assets/img/red_ghost.png');
         this.load.spritesheet('smallGum', 'assets/img/small_gum.png');
         this.load.spritesheet('bigGum', 'assets/img/big_gum.png');
     },
